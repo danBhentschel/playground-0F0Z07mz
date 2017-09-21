e013f84a063967a6d9ee0bd7f8f60a81bd0b96b7
danBhentschel <dan@hentschels.com>
2017-09-21 13:19:05 -0400

    Enhance VM assistant with color

--- work/start_vm.sh
+++ work/start_vm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+YELLOW='\033[1;33m'
+NOCOLOR='\033[0m'
+
 echo
 echo Preparing to start VM...
 echo
@@ -13,9 +16,11 @@ if [ -e /project/target/prepare_vm.sh ]; then
     /project/target/prepare_vm.sh ${1}
 fi
 
+echo -ne ${YELLOW}
 echo You might want to take some time now
 echo to preread the instructions below
 echo and to resize the terminal window
+echo -ne ${NOCOLOR}
 echo
 
 /start_vm.sh
