bd10cee6bf24d6b63807add2264c4810ce9e3ea4
danBhentschel <dan@hentschels.com>
2017-09-21 13:54:37 -0400

    Enhance VM assistant with color

--- script_images/color.sh
+++ script_images/color.sh
@@ -17,3 +17,7 @@ cmdecho ()
 {
     echo -e "${COLOR} ==> Next command: ${CMDCOLOR}${1}${COLOR} <==${NOCOLOR}"
 }
+
+if [ -z "$(tail -n 1 ~/.bashrc | grep PS1)" ]; then
+    echo "PS1='${debian_chroot:+($debian_chroot)}\[\e[38;5;28m\]\u@\h\[\033[00m\]:\[\e[38;5;61m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
+fi
