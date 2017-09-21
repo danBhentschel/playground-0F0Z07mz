afbc04ec939a1731ad23f1346160f89f5b991e19
danBhentschel <dan@hentschels.com>
2017-09-21 10:43:08 -0400

    Enhance VM assistant

--- work/start_vm.sh
+++ work/start_vm.sh
@@ -13,4 +13,9 @@ if [ -e /project/target/prepare_vm.sh ]; then
     /project/target/prepare_vm.sh ${1}
 fi
 
+echo You might want to take some time now
+echo to read over the instructions below
+echo and to resize the terminal window
+echo echo
+
 /start_vm.sh
